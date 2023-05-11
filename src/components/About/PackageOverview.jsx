78bfada9c3531003e68d374534ed566cdc0c34f1
= <dhakatedeep909@gmail.com>
2023-05-11 20:31:45 +0530

    Update

--- src/components/About/PackageOverview.jsx
+++ src/components/About/PackageOverview.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import blackbox from "./../../images/box.png";
 
 function PackageOverview() {
   return (
-    <div className="packageoverview">
-      <div className="po--description">
-        We've got you
-        <div className="po--d-covered">covered</div>
-        <div className="po--d--details">
-          In today's rapidly evolving threat landscape, it's more important than
+    <section>
+      <div className="packageoverview">
+        <div className="po--description">
+          Black Box
+          <div className="po--d-covered">Testing</div>
+          <div className="po--d--details">
+            At InTSecurity, we offer comprehensive black box testing services to
+            identify vulnerabilities from an outsider's perspective. We believe
+            that effective security requires a proactive approach. Our black box
+            testing services are designed to uncover vulnerabilities from the
+            outside, enabling you to fortify your defenses, protect your
+            valuable assets, and maintain the trust of your customers, without
+            even sharing a single line of code.
+            {/* In today's rapidly evolving threat landscape, it's more important than
           ever to have a robust and reliable security solution in place to
           protect your business. INT{"{Security}"} offer a wide range of
           security solutions and services to help businesses of all sizes
           mitigate risks, detect and respond to threats, and stay compliant with
-          industry regulations. Check out pricing for more details.
-        </div>
-        <button className="about--pricing">
+          industry regulations. Check out pricing for more details. */}
+          </div>
+          {/* <button className="about--pricing">
           {" "}
           Pricing <span>&#x2192;</span>
-        </button>
+        </button> */}
+        </div>
+        <div className="blackbox">
+          <img className="blackbox--main" src={blackbox} alt="blackbox"></img>
+          <span className="box--shadow"></span>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
---
+++ src/components/About/Why-intsecurity.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import question from "./../../images/questionmark.png";
+
+function Why() {
+  return (
+    <div className="about--why">
+      <div className="why--img">
+        <img src={question} className="blackbox--main question--img"></img>
+        <span className="box--shadow"></span>
+      </div>
+      <div className="po--description why--content ">
+        <div className="why--content--head">Why</div>
+        <div className="why--content--head">
+          INT<span className="font">{"{"} </span>SECURITY
+          <span className="font">{"}"}?</span>
+        </div>
+        <div className="why--main">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos omnis
+          nesciunt aperiam nostrum ducimus magni corporis cum nulla ad deleniti
+          mollitia ratione soluta quasi, non debitis aliquid sapiente nemo
+          quisquam iusto, voluptate tempore dolores, provident unde! Asperiores
+          voluptates accusantium ipsum odio velit iste, architecto et ipsam
+          excepturi consectetur minima eum?
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Why;
