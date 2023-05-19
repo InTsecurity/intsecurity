9282b029f8737bbe373db3ab8fe64aaaa7b60733
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-05-19 17:50:40 +0530

    Update mail.jsx

--- src/connections/mail.jsx
+++ src/connections/mail.jsx
@@ -1,5 +1,4 @@
-// Client ID 681809056083
-// Secret ID 019ec2c9ba5a4efa9b1890f53c4b4f648dc4c861f4afd7d5aa
+
 //  API Key eb320ce16ab8b120159906958e14b668-us8
 
 const mailchimpClient = require("@mailchimp/mailchimp_transactional")(
